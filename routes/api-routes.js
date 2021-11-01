4f3d19ad6c4877d370244ff80cec3906c4428053
zugzw4ng <bongcloud2001@gmail.com>
2021-11-01 19:53:29 -0400

    added update existing workout request

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -25,11 +25,22 @@ router.get("/api/workouts/range", (req, res) => {
 
 router.post("/api/workouts", ({ body }, res) => {
     db.Workout.create(body)
-      .then(dbWorkout => {
-        console.log(dbWorkout)
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+        .then(dbWorkout => {
+            console.log(dbWorkout)
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } }, { new: true, runValidators: true })
+        .then(dbWorkout => {
+            console.log(dbWorkout)
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
