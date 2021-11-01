7df424640e0f76b70918bc50f584541854fb0876
zugzw4ng <bongcloud2001@gmail.com>
2021-11-01 12:34:47 -0400

    refactored server.js syntax

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 const db = require("./models");
-const { application } = require("express");
 const app = express();
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker",
