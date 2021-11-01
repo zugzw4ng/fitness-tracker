ad239d32c500c1e9824009319022211c49349963
zugzw4ng <bongcloud2001@gmail.com>
2021-11-01 19:33:10 -0400

    added workout duration and toJSON to Workout model

--- models/Workout.js
+++ models/Workout.js
@@ -2,24 +2,33 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    day: {
-        type: Date,
-        default: () => new Date()
-    },
-    exercises: [
-        {
-            type: { type: String, required: "type is required" },
-            name: { type: String, required: "name is required" },
-            duration: { type: Number, required: "duration is required" },
-            weight: { type: Number },
-            reps: { type: Number },
-            sets: { type: Number },
-            distance: { type: Number }
-        }]
-
+  day: {
+    type: Date,
+    default: () => new Date()
+  },
+  exercises: [
+    {
+      type: {type: String, required: "type is required"},
+      name: {type: String, required: "name is required"},
+      duration: {type: Number, required: "duration is required"},
+      weight: {type: Number},
+      reps: {type: Number},
+      sets: {type: Number},
+      distance: {type: Number}
+    }]
+},
+{
+  toJSON: {
+    virtuals: true
+  }
 });
 
-const Workout = mongoose.model("Workout", WorkoutSchema);
+WorkoutSchema.virtual("totalDuration").get(function() {
+  return this.exercises.reduce(function(total, exercise) {
+    return total + exercise.duration
+  }, 0)
+})
+
+const Workout = mongoose.model("Exercise", WorkoutSchema);
 
 module.exports = Workout;
-
