e3d58b031803ed402b18157c2e53eb8916368bda
zugzw4ng <bongcloud2001@gmail.com>
2021-11-01 19:40:54 -0400

    added get request for workout info/stats

--- models/Workout.js
+++ models/Workout.js
@@ -2,31 +2,31 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: () => new Date()
-  },
-  exercises: [
-    {
-      type: {type: String, required: "type is required"},
-      name: {type: String, required: "name is required"},
-      duration: {type: Number, required: "duration is required"},
-      weight: {type: Number},
-      reps: {type: Number},
-      sets: {type: Number},
-      distance: {type: Number}
-    }]
+    day: {
+        type: Date,
+        default: () => new Date()
+    },
+    exercises: [
+        {
+            type: { type: String, required: "type is required" },
+            name: { type: String, required: "name is required" },
+            duration: { type: Number, required: "duration is required" },
+            weight: { type: Number },
+            reps: { type: Number },
+            sets: { type: Number },
+            distance: { type: Number }
+        }]
 },
-{
-  toJSON: {
-    virtuals: true
-  }
-});
+    {
+        toJSON: {
+            virtuals: true
+        }
+    });
 
-WorkoutSchema.virtual("totalDuration").get(function() {
-  return this.exercises.reduce(function(total, exercise) {
-    return total + exercise.duration
-  }, 0)
+WorkoutSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce(function (total, exercise) {
+        return total + exercise.duration
+    }, 0)
 })
 
 const Workout = mongoose.model("Exercise", WorkoutSchema);
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,11 +3,22 @@ const db = require("../models");
 
 router.get("/api/workouts", (req, res) => {
     db.Workout.find({})
-      .then(dbWorkout => {
-        console.log(dbWorkout)
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+        .then(dbWorkout => {
+            console.log(dbWorkout)
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({}).sort({ "day": -1 }).limit(7)
+        .then(dbWorkout => {
+            console.log(dbWorkout)
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
